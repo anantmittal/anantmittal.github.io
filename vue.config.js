6623b7aab17ee5a382e9b77d87f6da5133252634
Anant Mittal <anmittal@cs.washington.edu>
2021-07-15 12:40:57 -0700

    updated vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  runtimeCompiler: true,
-  publicPath:
+	runtimeCompiler: true,
+	publicPath: "/",
+	/*publicPath:
     process.env.NODE_ENV === "production"
       ? "/" + process.env.CI_PROJECT_NAME + "/"
-      : "/",
+      : "/",*/
 };
