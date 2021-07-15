cdfba56479289b1c963b554b331c7a9c13537dc4
Anant Mittal <anmittal@cs.washington.edu>
2021-07-15 16:24:14 -0700

    added links to pdfs

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,24 +4,39 @@
 			I am a Ph.D. student at the Paul G. Allen School of Computer Science
 			& Engineering, University of Washington, currently affiliated with
 			the
-			<a href="http://centerforgamescience.org/" target="_blank"
-				>Center for Game Science</a
-			>
+			<strong>
+				<a href="http://centerforgamescience.org/" target="_blank"
+					>Center for Game Science</a
+				>
+			</strong>
 			and closely collaborating with education researchers at
-			<a href="https://www.enlearn.org/" target="_blank">Enlearn</a>.
+			<strong>
+				<a href="https://www.enlearn.org/" target="_blank"
+					>Enlearn
+				</a> </strong
+			>.
 		</p>
 		<p>
 			At UW, my research has been at the intersection of human-computer
 			interaction, cognitive psychology, learning sciences, and applied
 			artificial intelligence. Using concepts from these fields, I try to
-			build personalized and interactive math learning systems for
-			classroom environments.
+			build personalized and interactive
+			<strong>
+				<a
+					href="https://homes.cs.washington.edu/~anmittal/EF+Math.pdf"
+					target="_blank"
+					>math learning</a
+				>
+			</strong>
+			systems for classroom environments.
 		</p>
 		<p>
 			Previously, I spent some time building data science pipelines to
 			generate interactive lessons that improve neuron tracing behavior of
-			<a href="https://www.mozak.science/landing" target="_blank"
-				>Mozak</a
+			<strong
+				><a href="https://www.mozak.science/landing" target="_blank"
+					>Mozak</a
+				></strong
 			>
 			players.
 		</p>
