3bd01990523c73e1ebe4057fef3865568dd891d1
Anant Mittal <anmittal@cs.washington.edu>
2021-07-15 15:29:04 -0700

    updated aboutme and added readme

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,12 +8,22 @@
 				>Center for Game Science</a
 			>
 			and closely collaborating with education researchers at
-			<a href="https://www.enlearn.org/" target="_blank">Enlearn</a>. My
-			research is at the intersection of human-computer interaction,
-			cognitive psychology, learning sciences, and applied artificial
-			intelligence. Using concepts from these fields, I try to build
-			personalized and interactive math learning systems for classroom
-			environments.
+			<a href="https://www.enlearn.org/" target="_blank">Enlearn</a>.
+		</p>
+		<p>
+			At UW, my research has been at the intersection of human-computer
+			interaction, cognitive psychology, learning sciences, and applied
+			artificial intelligence. Using concepts from these fields, I try to
+			build personalized and interactive math learning systems for
+			classroom environments.
+		</p>
+		<p>
+			I also spent some time building data science pipelines to generate
+			interactive lessons for improving neuron tracing behavior of
+			<a href="https://www.mozak.science/landing" target="_blank"
+				>Mozak</a
+			>
+			players.
 		</p>
 	</div>
 </template>
